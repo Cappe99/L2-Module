82b22b62e62ca1ed29ec36e5caf190a65d211f62
Casper Björlin <cb224cd@student.lnu.se>
2025-09-14 11:06:45 +0200

    Remove items from cart and bugfix in add items

--- src/cart.js
+++ src/cart.js
@@ -7,7 +7,31 @@ export class Cart {
          if (quantity === undefined) {
             quantity = 1
         }
-        this.items.push({ ...product, quantity })
+        const existingItem = this.items.find(item => item.id === product.id)
+
+        if (existingItem) {
+            existingItem.quantity += quantity
+        } else {
+            this.items.push({ ...product, quantity })
+        } 
+    }
+
+    removeProductFromCart(product, quantity) {
+        if (quantity === undefined) {
+            quantity = 1
+        }
+
+       const index = this.items.findIndex(item => item.id === product.id)
+       
+       if (index !== -1) {
+        const cartItem = this.items[index]
+
+        cartItem.quantity -= quantity
+
+        if (cartItem.quantity <= 0) {
+            this.items.splice(index, 1)
+        }
+       }
     }
 
     getTotalPrice() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import { Cart } from "./cart.js";
 const cart = new Cart()
 
 cart.addProductToCart({ id: 1, name: "T-shit", price: 200}, 3)
+cart.addProductToCart({ id: 1, name: "T-shit", price: 200}, 3)
 cart.addProductToCart({ id: 12, name: "Keps", price: 199})
+
+
+cart.removeProductFromCart({ id: 1})
 
 
 console.log(cart.items)
